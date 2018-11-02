5c908bcb8fd4ea4a8317858c5320dc5ea42e4e67
RWalsweer <renewalsweer@gmail.com>
2018-11-02 14:14:43 +0100

    Refactoring gedaan op de code

--- App.js
+++ App.js
@@ -11,34 +11,38 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 
+    items: [
+        {    
+        xtype: 'container',             // defineren van een container via xtype.  fullname is : Ext.container.Container
+        itemId:'pulldown-container',    // deze container geeft ons controle over de layout van de comboboxen
+        layout: {
+            type: 'hbox',
+            align: 'stretch'
+            }
+        }
+    ],
+
+
     // aangeven welke objecten er op de app aanwezig zijn, beter leesbaarheid wanneer iemand er naar kijkt.
-    myStore: undefined,
+    defectStore: undefined,
     myGrid: undefined,
 
     launch: function() {
 
 	// melding schrijven naar de console van de browser	
 	console.log('Starten van de applicatie');
-
-    // this.  wil zeggen dat je het item op app niveau declareert
-    //
-    this.pulldownContainer = Ext.create('Ext.container.Container', {
-        layout: {
-            type: 'hbox',
-            align: 'stretch'
-        },
-    });
-
-    // toevoegen van de container aan de app en deze tonen
-    this.add(this.pulldownContainer);
-    
+ 
     this._loadIterations();
 
     },
 
+
     // Laden van de Iterations combobox
     _loadIterations() {
-        this.iterComboBox = Ext.create('Rally.ui.combobox.IterationComboBox' , {
+        var me = this;                              // Gebruik me om aan te geven dat je verwijst naar de app (hoogste niveau)
+
+        var iterComboBox = Ext.create('Rally.ui.combobox.IterationComboBox' , {
+            itemId: 'iteration-combobox',
             // Naam en positie van de label voor de Combobox
             fieldLabel: 'Iteration :',
             labelAlign: 'right',
@@ -47,20 +51,10 @@ Ext.define('CustomApp', {
             width: 400,
 
             listeners: {
-                // event wanneer de combobox is opgebouwd.
-                ready: function(combobox) {
-                //  console.log('Gekozen record by Ready Event', combobox.getRecord());
-                //  var selectedIterRef2 = combobox.getRecord().get('_ref');
-                //  console.log('Geselecteerde record bij Ready Event ', selectedIterRef2);
-                    this._LoadSevereties();
-                },
-
-                // event wanneer er iets wordt geslecteerd in de combobox
-                select: function(combobox, records) {
-                    this._LoadData();
-                },
-
-                scope: this
+                ready: me._onIterationsReady,     // event wanneer de combobox is opgebouwd.
+                                                    // omdat er data wordt doorgegeven dan ook this._LoadServerities hier worden neergezet
+                select: me._LoadData,             // event wanneer er data wordt geselecteerd.
+                scope: me
             }
 
         });
@@ -68,70 +62,103 @@ Ext.define('CustomApp', {
         //this.add(this.iterComboBox);
 
         // Tonen van de Combobox in de pulldownContainer
-        this.pulldownContainer.add(this.iterComboBox);
+        //this.pulldownContainer.add(this.iterComboBox);
+
+        // this is de app en met down gaan we in de app zoeken
+        //var container = this.down('#pulldown-container');
+        //console.log('Gezochte container met down', container);
+        me.down('#pulldown-container').add(iterComboBox);
+
     },
 
+
+    _onIterationsReady: function(combobox, eOpts) {
+        var me = this;
+        me._LoadSeverities();
+    },
+
+
     // Laden van de Combobox Severities
-    _LoadSevereties: function() {
-        this.severityComboBox = Ext.create('Rally.ui.combobox.FieldValueComboBox', {
+    _LoadSeverities: function() {
+        var me = this;
+
+        var severityComboBox = Ext.create('Rally.ui.combobox.FieldValueComboBox', {
+            itemId: 'severity-combobox',
             model: 'Defect',
             field: 'Severity',
 
             fieldLabel: 'Severity :',
             labelAlign: 'right',
             listeners: {
-                ready: function(combobox) {
-                    this._LoadData();
-                },
-                select: function(combobox, records) {
-                    this._LoadData();
-                },
-            scope: this
+                ready: me._LoadData,
+                select: me._LoadData,
+                scope: me
             }
         });
 
-        this.pulldownContainer.add(this.severityComboBox);
+        me.down('#pulldown-container').add(severityComboBox);
+    },
+
+    // Aanmaken van de filters voor de Grid met de iteration & sevirity waardes
+    _getFilters: function(iterationValue, severityValue) {
+        // aanmaken van filter op de Iteration Combobox
+        var iterationFilter = Ext.create('Rally.data.wsapi.Filter' , {
+                property: 'Iteration',
+                operation: '=',
+                value: iterationValue
+        });
+        //console.log('Iteration filter' , iterationFilter, iterationFilter.toString());
+
+        // aanmaken van een filter op de Severity Combobox
+        var severityFilter = Ext.create('Rally.data.wsapi.Filter', {
+                property: 'Severity',
+                operation: '=',
+                value: severityValue
+        });
+
+
+        // aanmaken van een extra filter 
+        var blockedFilter = Ext.create('Rally.data.wsapi.Filter', {
+                property: 'Blocked',
+                operation: '=',
+                value: true
+        });
+
+        // Aan elkaar koppelen van de filters
+        // in dit geval via een AND  Zie Rally.data.wsapi.Filter voor meer opties zoals OR
+        return iterationFilter.and(severityFilter);
+
+        // Koppelen van meerdere filters met een OR optie
+        // Wordt verder niet gebruikt in de app maar staat er als een voorbeeld.
+        //var myComboFilters = blockedFilter.or(myFilters);
+        //console.log('My filters gecombineerd', myComboFilters.toString());
+
     },
 
     // Laden van de Store en Data uit Rally
     _LoadData: function() {
+        var me = this;
+        console.log('Wie is me', me); 
 
         // tonen van de geselecteerde record (referentie)
-        var selectedIterRef = this.iterComboBox.getRecord().get('_ref');
-        console.log('Geselecteerde record bij Ready Event ', selectedIterRef);
-
-        // checken wat je exact terugkrijgt vanuit de combobox (severity)
-        //var selectedSeverityValue2 = this.severityComboBox.getRecord();
-        //console.log('Sevirity combobox select record', selectedSeverityValue2);
-
+        var selectedIterRef = me.down('#iteration-combobox').getRecord().get('_ref');
         // ophalen waarde uit de sevirity combobox
-        var selectedSeverityValue = this.severityComboBox.getRecord().get('value');
+        var selectedSeverityValue = me.down('#severity-combobox').getRecord().get('value');
 
-        // aanmaken van een array met filters die gebruikt kunnen worden in de store om de deze te kunnen filteren
-        var myFilters = [
-            // filter op Waarde  uit de Iteration combobox
-            {
-                property: 'Iteration',
-                operation: '=',
-                value: selectedIterRef
-            },
-            // filter op Waarde  uit de Sevirity combobox
-            {
-                property: 'Severity',
-                operation: '=',
-                value: selectedSeverityValue
-            }
-        ];
+        var myFilters = me._getFilters(selectedIterRef, selectedSeverityValue);
+        console.log('mijn Filters', myFilters.toString())
+
+
 
         // Bepalen of de store al bestaat.  Zoja refreshen, Zonee aanmaken
-        if (this.defectStore) {
+        if (me.defectStore) {
 
-            this.defectStore.setFilter(myFilters);
-            this.defectStore.load();
+            me.defectStore.setFilter(myFilters);
+            me.defectStore.load();
         } else {
 
             // Aanmaken van een Store  (records of data)
-            this.defectStore = Ext.create('Rally.data.wsapi.Store', {
+            me.defectStore = Ext.create('Rally.data.wsapi.Store', {
                 // Welk type data wil je uit Rally halen
                 model: 'defect',
                 // Geeft aan dat er direct data wordt geladen nadat de store actief is geworden.
@@ -146,13 +173,13 @@ Ext.define('CustomApp', {
                     // opgehaalde data plaatsen in een Grid
                     // Op deze plaats zetten, anders wordt de grid niet geladen.
                     // Wel checken of de grid al is aangemaakt, anders krijg je 2 grids.
-                        if (!this.myGrid) {                 // ! beteken ongelijk/niet aanwezig
-                         this._createGrid(myStore)
+                        if (!me.myGrid) {                 // ! beteken ongelijk/niet aanwezig
+                         me._createGrid(myStore)
                         }
                     },
                     
                     // app op zichtzelf toevoegen aan de load function zodat de Grid zal worden getoond. 
-                    scope: this
+                    scope: me
                 },
                 // Aangeven welke velden je uit Rally wil halen.
                 fetch: ['FormattedID', 'Name', 'Severity', 'Iteration']
@@ -162,7 +189,9 @@ Ext.define('CustomApp', {
 
     // Laden en tonen van de Grid
     _createGrid: function(myStoryStore) {
-        this.myGrid = Ext.create('Rally.ui.grid.Grid', {
+        var me = this;
+        
+        me.myGrid = Ext.create('Rally.ui.grid.Grid', {
             store: myStoryStore,
             columnCfgs: [
                 // Kolomnamen moeten voorkomen in de fetch
@@ -174,8 +203,8 @@ Ext.define('CustomApp', {
         });
 
         // Manier om te kijken wat je exact terugkrijgt vanuit Rally
-        this.add(this.myGrid);
-        console.log('Wat is dit: ', this);
+        me.add(me.myGrid);
+        console.log('Wat is dit: ', me);
         }
 
 });
